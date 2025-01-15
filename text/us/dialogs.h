8a4bb454abd0e59cce7cf8725163397e2c037010
beemancer <59190135+beemancer@users.noreply.github.com>
2025-01-15 03:11:05 -0800

    Update dialogs.h

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -275,12 +275,12 @@ Shhh! Please walk\n\
 quietly in the hallway!"))
 
 DEFINE_DIALOG(DIALOG_020, 1, 6, 95, 150, _("\
-Dear Mario:\n\
-Please come to the\n\
-castle. I've baked\n\
-a cake for you.\n\
+Dear Epilogue Gaming:\n\
+Please come to my\n\
+birthday party. I have\n\
+bottomless mimosas for you.\n\
 Yours truly--\n\
-Princess Toadstool"))
+MissVVitch"))
 
 DEFINE_DIALOG(DIALOG_021, 1, 5, 95, 200, _("\
 Welcome.\n\
